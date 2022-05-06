74fe9374b8d66c1dca14e2c4f96c3e35cbbd133a
Riddy105 <99593252+Riddy105@users.noreply.github.com>
2022-05-06 11:21:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+// alert("hello");
+// let toggleClass = function(){
+//   document.querySelector(".faq-1").classList.toggle("open-state");
+
+// }
+
+document.querySelector(".wrapper-1").addEventListener("click", function () {
+  document.querySelector(".faq-1").classList.toggle("open-state");
+});
+document.querySelector(".wrapper-2").addEventListener("click", function () {
+  document.querySelector(".faq-2").classList.toggle("open-state");
+});
+document.querySelector(".wrapper-3").addEventListener("click", function () {
+  document.querySelector(".faq-3").classList.toggle("open-state");
+});
+document.querySelector(".wrapper-4").addEventListener("click", function () {
+  document.querySelector(".faq-4").classList.toggle("open-state");
+});
+document.querySelector(".wrapper-5").addEventListener("click", function () {
+  document.querySelector(".faq-5").classList.toggle("open-state");
+});
